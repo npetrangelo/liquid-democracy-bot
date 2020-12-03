5bcd72fa8027ad7e97e2227facd236dad31015da
Nathan Petrangelo <nathan@petrangelo.org>
2020-12-02 22:52:58 -0500

    Undo pointless diff

--- bot.js
+++ bot.js
@@ -13,6 +13,5 @@ function gotMessage(msg) {
     if (msg.author.bot) return;
     if (msg.channel.id == process.env.CHANNEL && msg.content === 'ping') {
         msg.channel.send('pong');
-        
     }
 }
